c20826ab2a612b8a6487b7f6f74f953a28a5b222
Graham Hukill <ghukill@gmail.com>
2019-01-29 17:26:44 -0500

    scaffolding contd

--- bookscanner.py
+++ bookscanner.py
@@ -1,5 +1,6 @@
 
 # generic
+import logging
 import os
 
 # modules
@@ -16,6 +17,14 @@ output_path = 'data/natural_light_done'
 '''
 
 
+# setup logger
+logging.basicConfig(level=logging.DEBUG)
+# parso shims
+logging.getLogger('parso.python.diff').disabled = True
+logging.getLogger('parso.cache').disabled = True
+logger = logging.getLogger(__name__)
+
+
 
 def process_issue_folder(input_path, output_path):
 
@@ -26,17 +35,20 @@ def process_issue_folder(input_path, output_path):
 
 	'''
 
-	# confirm folder
+
+	# handle input/output folders
 	if not os.path.exists(input_path) or not os.path.isdir(input_path):
 		raise Exception('%s either does not exist, or is not a directory' % input_path)
-
-
-	# create output folder
 	if not os.path.exists(output_path):
 		os.makedirs(output_path)
 
 
-	# crop
+	# loop through images
+	for page in os.listdir(input_path):
+
+		# page path
+		page_path = os.path.join(input_path, page)
+		logging.debug('working on page: %s' % page_path)
 
 
 

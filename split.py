f681bf354f66af7133690228ee2e689c22e9ed28
Graham Hukill <ghukill@gmail.com>
2019-02-02 14:50:06 -0500

    progress on splitting

--- split.py
+++ split.py
@@ -10,11 +10,19 @@ from PIL import Image
 import os
 
 
+# setup logger
+logging.basicConfig(level=logging.DEBUG)
+# parso shims
+logging.getLogger('parso.python.diff').disabled = True
+logging.getLogger('parso.cache').disabled = True
+logger = logging.getLogger(__name__)
+
 
 def split_page(
 	input_filepath,
 	cross_section_height=500,
-	kernel_size=3):
+	kernel_size=3,
+	mid_range=None):
 
 	'''
 	Focus on area roughly in the middle, 1500-2500
@@ -23,20 +31,35 @@ def split_page(
 
 
 	# read image from filepath
-	image = cv2.imread(input_filepath,0)
+	image = cv2.imread(input_filepath, 0)
 
 	# make sure double page
-	if image.shape[1] - image.shape[0] > 1000:
+	if image.shape[1] / image.shape[0] > 1.25:
 
 		# debug
-		print(input_filepath, image.shape)
+		metrics = {
+			'filepath':input_filepath,
+			'shape':image.shape,
+		}
 
+		# if cross section None, use full height
 		if cross_section_height == None:
 			cross_section_height = image.shape[0]
 
+		# set mid-section interval
+		'''
+		#######---|---#######
+		#######---|---#######
+		#######---|---#######
+		'''
+		if mid_range == None:
+			logger.debug('determining mid-range pixels')
+			mid_range = [int(image.shape[1]/2 - 200), int(image.shape[1]/2 + 200)]
+			metrics['mid_range'] = mid_range
+
 		# get horizontal gradient
 		sobelx = cv2.Sobel(
-			image[0:cross_section_height, 1500:2500],
+			image[0:cross_section_height, mid_range[0]:mid_range[1]],
 			cv2.CV_64F,
 			1,
 			0,
@@ -47,15 +70,26 @@ def split_page(
 		col_sums = np.sum(sobelx, axis=0)
 
 		# plot
-		# plt.plot(col_sums)
+		# plt.pyplot.plot(col_sums)
 
 		# get center line
-		cl_index = np.argmax(col_sums)
+		cl_index_rel = np.argmax(col_sums)
+		cl_index = mid_range[0] + cl_index_rel
+		metrics['cl_index'] = cl_index
 
 		# check
-		print( (cl_index + 1500) / image.shape[1])
+		metrics['halfway_percentage'] = cl_index / image.shape[1]
 
-		return (col_sums, cl_index)
+		# debug
+		logger.debug(metrics)
+		return metrics
+
+	else:
+		logger.debug('appears to be single page')
+		return {
+			'filepath':input_filepath,
+			'shape':image.shape,
+		}
 
 
 
